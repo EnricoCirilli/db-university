97dbb031b10e69e0409c1c48f3fa8a2db29e3540
EnricoCirilli <enrico.cirilli@gmail.com>
2023-12-20 14:17:40 +0100

    add: create table courses file text.sql

---
+++ text.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `courses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `degree_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `period` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` tinyint(3) UNSIGNED NOT NULL,
+  `cfu` tinyint(3) UNSIGNED NOT NULL,
+  `website` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
