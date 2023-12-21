a88863bed007e9ebabc098a1a084a8fddecb64a1
EnricoCirilli <enrico.cirilli@gmail.com>
2023-12-21 15:36:45 +0100

    add: quarta risposta a groupby.sql

--- groupby.sql
+++ groupby.sql
@@ -17,3 +17,7 @@ GROUP BY exam_id;
 
 
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `department_id`, COUNT(`name`) AS `total_courses`
+FROM `degrees`
+GROUP BY `department_id`;
