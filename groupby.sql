79c1797d757fa950d8148953d4d0afc283740e02
EnricoCirilli <enrico.cirilli@gmail.com>
2023-12-21 14:57:35 +0100

    add: groupby.sql seconda risposta

--- groupby.sql
+++ groupby.sql
@@ -1,10 +1,13 @@
-1. Contare quanti iscritti ci sono stati ogni anno
+1. Contare quanti iscritti ci sono stati ogni anno.
 
 SELECT YEAR(enrolment_date) AS anno, COUNT(*) AS num_iscritti
 FROM students
 GROUP BY anno;
 
-2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio.
+SELECT COUNT(DISTINCT id) AS num_insegnanti
+FROM teachers
+GROUP BY office_address;
 
 3. Calcolare la media dei voti di ogni appello d'esame
 
